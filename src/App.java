876f022097ea5a148e4d42d13fd1374f29ba858a
ronicley angelo <ronicley8@hotmail.com>
2023-03-30 15:08:17 -0300

    Gerando a figurinha com o texto

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 import GerarFigurinhas.GeradorDeFigurinhas;
 
+import java.awt.*;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -20,14 +23,35 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println("\u001b[38;5;214mTítulo:\u001b[m" + " 🎥 " + filme.get("title"));
-            System.out.println("\u001b[38;5;218mImagem:\u001b[m" + filme.get("image"));
+            // Print Terminal
             double classificao = Double.parseDouble(filme.get("imDbRating"));
             int numeroEstrela = (int) classificao;
-            System.out.println("\u001b[38;5;212mClassificação: \u001b[m" +  filme.get("imDbRating") );
-            for (int i = 1; i <= numeroEstrela ; i++) {
+
+            String texto = "";
+            if (numeroEstrela == 8 || numeroEstrela >= 10)  {
+                texto = "Ótimas Avaliações 👌";
+            } else if(numeroEstrela >= 7 || numeroEstrela == 6) {
+                texto = "Filme pouco recomendado 🤔";
+            } else {
+                texto = "Não recomendado 😿";
+            }
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            var geradora = new GeradorDeFigurinhas();
+            geradora.criar(inputStream, nomeArquivo, texto);
+
+            System.out.println("\u001b[38;5;214mTítulo:\u001b[m" + " 🎥 " + filme.get("title"));
+            System.out.println("\u001b[38;5;218mImagem:\u001b[m" + filme.get("image"));
+            System.out.println("\u001b[38;5;212mClassificação: \u001b[m" + filme.get("imDbRating"));
+
+            for (int i = 1; i <= numeroEstrela; i++) {
                 System.out.print("⭐");
             }
+
             System.out.println("\n");
         }
     }
--- src/GerarFigurinhas/GeradorDeFigurinhas.java
+++ src/GerarFigurinhas/GeradorDeFigurinhas.java
@@ -5,39 +5,31 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.awt.Font;
-import java.io.FileInputStream;
 import java.io.InputStream;
-import java.net.URL;
 
 public class GeradorDeFigurinhas {
-    public void criar() throws Exception {
+    public void criar(InputStream inputStream, String nomeArquivo, String texto) throws Exception {
 
         //leitura da imagem
-        InputStream inputStream = new URL("https://m.media-amazon.com/images/M/MV5BY2Q2NDI1MjUtM2Q5ZS00MTFlLWJiYWEtNTZmNjQ3OGJkZDgxXkEyXkFqcGdeQXVyNTI4MjkwNjA@.jpg").openStream();
-//       InputStream inputStream = new FileInputStream("./src/image/DancesWith.jpg");
         BufferedImage imageOriginal = ImageIO.read(inputStream);
 
         // criar nova imagem em memória com transparência e com tamnanho novo
-        Integer largura   = imageOriginal.getWidth() + 200;
+        Integer largura   = imageOriginal.getWidth() + 300;
         Integer altura    = imageOriginal.getHeight();
-        Integer newAltura = altura + 200;
+        Integer newAltura = altura + 300;
         BufferedImage novaImage = new BufferedImage(largura,newAltura, BufferedImage.TRANSLUCENT);
 
         //copiar a imagem original para nova imagem (em memória)
         Graphics2D graphics2D = (Graphics2D) novaImage.getGraphics();
         graphics2D.drawImage(imageOriginal, 0,0, null);
 
-        var fonte = new Font(Font.MONOSPACED, Font.HANGING_BASELINE, 89);
+        var fonte = new Font(Font.MONOSPACED, Font.HANGING_BASELINE, 88);
         graphics2D.setFont(fonte);
-        graphics2D.setColor(Color.MAGENTA);
-        graphics2D.setBackground(Color.CYAN);
+        graphics2D.setColor(Color.YELLOW);
+        graphics2D.setBackground(Color.darkGray);
 
         // escrever uma nova imagem
-        graphics2D.drawString("Default is about", 0, newAltura - 100);
-        ImageIO.write(novaImage,"png", new File("./src/saida/.png"));
-    }
-    public static void main (String[] args) throws Exception {
-        var geradora = new GeradorDeFigurinhas();
-        geradora.criar();
+        graphics2D.drawString(texto, 0, newAltura - 90);
+        ImageIO.write(novaImage,"png", new File("./src/saida/" + nomeArquivo));
     }
 }
